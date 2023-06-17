10a9e53a81a3da951980099502a4851df995b2fe
victorhrdsilva <victorhrdsilva@gmail.com>
2023-06-17 11:34:02 -0300

    feat: add retrieve all  programmers and by id router and controller

--- src/controllers/programmerControllers.js
+++ src/controllers/programmerControllers.js
@@ -2,20 +2,45 @@ import programmer from "../database/tables/programmer.js";
 import { STATUS_CODE } from "../enums/statusCode.js";
 
 const createProgrammer = async (req, res) => {
-    const { name, python, javascript, java } = req.body;
+  const { name, python, javascript, java } = req.body;
 
-    try {
-        const newProgrammer = await programmer.create({
-            name: name,
-            python: python,
-            javascript: javascript,
-            java: java
-        });
+  try {
+    const newProgrammer = await programmer.create({
+      name: name,
+      python: python,
+      javascript: javascript,
+      java: java,
+    });
 
-        res.status(STATUS_CODE.OK).send(newProgrammer);
-    } catch (error) {
-        res.status(STATUS_CODE.SERVER_ERROR).send(error);
-    };
+    res.status(STATUS_CODE.OK).send(newProgrammer);
+  } catch (error) {
+    res.status(STATUS_CODE.SERVER_ERROR).send(error);
+  }
 };
 
-export {createProgrammer}
+const getProgrammers = async (req, res) => {
+  try {
+    const records = await programmer.findAll();
+    res.status(STATUS_CODE.OK).send(records);
+  } catch (error) {
+    res.status(STATUS_CODE.SERVER_ERROR).send(error);
+  }
+};
+
+const getProgrammerById = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const record = await programmer.findByPk(id);
+
+    if (record) {
+      res.status(STATUS_CODE.OK).send(record);
+    } else {
+      res.status(STATUS_CODE.OK).send("No programmer found using received ID");
+    }
+  } catch (error) {
+    res.status(STATUS_CODE.SERVER_ERROR).send(error);
+  }
+};
+
+export { createProgrammer, getProgrammers, getProgrammerById};
--- src/routers/programmerRouters.js
+++ src/routers/programmerRouters.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createProgrammer } from '../controllers/programmerControllers.js';
+import { createProgrammer, getProgrammerById, getProgrammers } from '../controllers/programmerControllers.js';
 import { validateSchema } from '../middlewares/validateSchema.js';
 import { programmerSchema } from '../schemas/programmerSchema.js';
 const router = express.Router();
 
 router.post('/crateProgrammer', validateSchema(programmerSchema), createProgrammer);
+
+router.get('/retrieveProgrammers', getProgrammers)
+router.get('/retrieveProgrammer/:id', getProgrammerById)
+
 
 export default router;
